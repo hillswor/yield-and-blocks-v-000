83455a8d8660f271dbbb7f8785377c355fb36a5d
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-24 22:04:32 +0000

    automatically backed up by learn

--- blocks.rb
+++ blocks.rb
@@ -1,9 +1,11 @@
 def aliens
-  i = 1
-  j = 2
-  yield(i, j)
-  i = "magick"
-  return nil
+  if block_given?
+    i = 1
+    j = 2
+    yield(i, j)
+  else  
+    puts "magick"
+  end
 end
 
 aliens {|x, y| puts x}
